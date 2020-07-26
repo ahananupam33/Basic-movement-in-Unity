117a6d148df7c88540326b664ac0b231d83a757e
ahananupam33 <68801494+ahananupam33@users.noreply.github.com>
2020-07-26 13:54:02 +0530

    Add files via upload
    Assets for movement

---
+++ Assets/Camera_Follower.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Diagnostics;
+using System.Security.Cryptography;
+using UnityEngine;
+
+public class Camera_Follower : MonoBehaviour
+{
+    public Transform player;
+    public Vector3 camDistance; 
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = player.position + camDistance;
+    }
+}
---
+++ Assets/Player_Script.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Reflection.Emit;
+using System.Threading;
+using UnityEngine;
+
+public class Player_Script : MonoBehaviour
+{
+    public Rigidbody rb;
+    // Start is called before the first frame update
+    public float movementForce = 500f;
+    // Update is called once per frame
+    void Update()
+    {
+        
+
+        if (Input.GetKey("right"))
+            rb.AddForce(movementForce * Time.deltaTime, 0, 0);
+        if (Input.GetKey("left"))
+            rb.AddForce(-movementForce * Time.deltaTime, 0, 0);
+        if (Input.GetKey("up"))
+            rb.AddForce(0, 0, movementForce * Time.deltaTime);
+        if (Input.GetKey("down"))
+            rb.AddForce(0, 0, -movementForce * Time.deltaTime);
+
+    }
+}
